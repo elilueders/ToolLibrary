37e7ccbfff7297c8d4a91e7160a5b9ef6be43346
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2020-11-13 16:50:14 -0600

    viewAllTools update 1.0
    Wrote code for viewAllTools, added to webcontroller and added link in index.

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -9,6 +9,22 @@
 
 package dmacc.controller;
 
-public class WebController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
+public class WebController {
+	@GetMapping({ "/", "viewAllTools" })
+	public String viewAllTools(Model model) {		
+		model.addAttribute("tool", repo.findAll());
+		return "viewAllTools";
+	}
+	
+	@PostMapping("/borrow/{id}")
+	public String reviseTool(Tool t, Model model) {
+		repo.save(t);
+		return viewAllTools(model);
+	}
 }
